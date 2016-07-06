10df45abd9a7d645f9737c60f25b5af9197cc390
muthuselvakumari <muthuselvakumari.m@gmail.com>
2016-07-06 12:01:06 +0530

    program1.c

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+int n;
+printf("\n enter the number to be checked:");
+scanf("%d",&n);
+if(n>0)
+{printf("\n the given number is positive");}
+if(n<0)
+printf("\n the given number is negative");
+else
+printf("\n the given number is zero");}
+}
